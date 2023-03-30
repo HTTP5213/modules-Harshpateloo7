21c7bd42263217decfd5b8639d2b79e0f8fea3fc
Harshadkumar Patel <64983530+Harshpateloo7@users.noreply.github.com>
2023-03-30 19:28:39 -0400

    import and export commit

---
+++ public/my-module.js
@@ -0,0 +1,4 @@
+import {theShire, buckland}  from './another-module.js';
+export var statement = () => `${theShire.name} was founded ${buckland.founding - theShire.founding} years before ${buckland.name}.`;
+export {theShire}
+
