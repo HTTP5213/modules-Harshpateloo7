7666468676aed893601f8bbbd02d3895ee44407e
Harshadkumar Patel <64983530+Harshpateloo7@users.noreply.github.com>
2023-03-30 20:57:18 -0400

    Update my-module.test.js

--- test/my-module.test.js
+++ test/my-module.test.js
@@ -3,12 +3,12 @@ import { theShire, statement } from '../public/my-module.js';
 import { expect } from 'chai';
 describe('The Shire', function() {
     it('should be Hobbit Settlement', function() {
-        expect(theShire).to.be.an.instanceof(HobbitSettlement);
+      expect(theShire).to.be.an.instanceof(HobbitSettlement);
     });
 });
 describe('statement', function() {
 	it('Should return the correct string', () => {
-	expect(statement()).to.equal('The Shire was founded 739 years before Buckland.');
+	  expect(statement()).to.equal('The Shire was founded 739 years before Buckland.');
 	
     });
 });
